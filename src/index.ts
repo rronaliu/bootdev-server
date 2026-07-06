c2d9de1c2215ae249f89a3b2b7c60acef674c6db
Rron Aliu <rronaliu95@gmail.com>
2026-07-06 22:01:29 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+
+const handlerReadiness = (req: Request, res: Response) => {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send("OK");
+};
+
+app.get("/healthz", handlerReadiness);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
